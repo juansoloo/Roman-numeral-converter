08a988164f7d5697c302bb194ab65628cb9f4633
Juan hurtado <128935594+juansoloo@users.noreply.github.com>
2024-03-06 20:33:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ const output = document.getElementById('output');
 
 
 
-convertBtn.addEventListener('click', function() 
-{
- const inputVal = parseInt(input.value, 10);
+convertBtn.addEventListener('click', function() {
+const inputVal = parseInt(input.value, 10);
 
-if (inputVal > 0 && inputVal < 4000) 
-{     
+if (inputVal > 0 && inputVal < 4000) {     
     const stringToNum = String(inputVal); 
     const splitNum = stringToNum.split("").map(num => parseInt(num, 10));
 
@@ -24,106 +22,72 @@ if (inputVal > 0 && inputVal < 4000)
    
     
 // Thousands 
-    if (a >= 1000)
-    {
-        for (let i = 0; i < splitNum[splitNum.length - 4]; i++) 
-    {
-        numeral += 'M';
+    if (a >= 1000) {
+        for (let i = 0; i < splitNum[splitNum.length - 4]; i++) {
+            numeral += 'M';
         }
     }
 // hundreds  
-    if (b > 0)
-    {
-    if (b <= 300) 
-    {
-       // numeral = ''
-        for (let i = 0; i < splitNum[splitNum.length - 3]; i++)
-        {
-            numeral += 'C';        
+    if (b > 0) {
+        if (b <= 300) {
+            for (let i = 0; i < splitNum[splitNum.length - 3]; i++) {
+                numeral += 'C';        
+            }
+        } else if (b >= 400 && b < 500) {
+            numeral += 'CD';
+        } else if (b >= 500 && b < 900) {
+            numeral += 'D'
+            for (let i = 5; i < splitNum[splitNum.length - 3]; i++) {
+                numeral += 'C';
+            }
+        } else if (b >= 900 && b < 1000) {
+            numeral += 'CM';
         }
-    } 
-    else if (b >= 400 && b < 500)
-    {
-        numeral += 'CD';
-    } 
-    else if (b >= 500 && b < 900)  
-    {
-        numeral += 'D'
-        for (let i = 5; i < splitNum[splitNum.length - 3]; i++) 
-        {
-        numeral += 'C';
-        }
-    }
-    else if (b >= 900 && b < 1000)
-    {
-        numeral += 'CM';
-    }
     }
 // tens
-    if (c > 0) 
-    {
-        if (c <= 30) 
-        {
-            for (let i = 0; i < splitNum[splitNum.length - 2]; i++)
-            {
+    if (c > 0) {
+        if (c <= 30) {
+            for (let i = 0; i < splitNum[splitNum.length - 2]; i++) {
                 numeral += 'X';        
             }
-        } 
-        else if (c >= 40 && c < 50)
-        {
+        } else if (c >= 40 && c < 50) {
             numeral += 'XL';
-        } 
-        else if (c >= 50 && c < 90)  
-        {
+        } else if (c >= 50 && c < 90) {
             numeral += 'L';
-            for (let i = 5; i < splitNum[splitNum.length - 2]; i++) 
-            {
-            numeral += 'X';
+            for (let i = 5; i < splitNum[splitNum.length - 2]; i++) {
+                numeral += 'X';
             }
-        }
-        else if (c >= 90 && c < 100)
-        {
-            numeral += 'XC';
+        } else if (c >= 90 && c < 100) {
+        numeral += 'XC';
         }  
     }
-//ones 
-    if (d > 0) 
-{
-    if (d <= 3) 
-    {
-        for (let i = 0; i < splitNum[splitNum.length - 1]; i++)
-        {
-            numeral += 'I';        
-        }
-    } 
-    else if (d >= 4 && d < 5)
-    {
-        numeral += 'IV';
-    } 
-    else if (d >= 5 && d < 9)  
-    {
-        numeral += 'V';
-        for (let i = 5; i < splitNum[splitNum.length - 1]; i++) 
-        {
-        numeral += 'I';
-        }
-    }
-    else if (d >= 9 && d < 10)
-    {
-        numeral += 'IX';
-    }  
+//one 
+    if (d > 0) {
+        if (d <= 3) {
+            for (let i = 0; i < splitNum[splitNum.length - 1]; i++) {
+                numeral += 'I';        
+            }
+        } else if (d >= 4 && d < 5) {
+            numeral += 'IV';    
+        } else if (d >= 5 && d < 9) {
+            numeral += 'V';
+            for (let i = 5; i < splitNum[splitNum.length - 1]; i++) {
+                numeral += 'I';
+            }
+        } else if (d >= 9 && d < 10) {
+            numeral += 'IX';
+        }  
     }
 
     output.innerHTML = numeral;
    
-} else if (inputVal < 0) {
-    output.innerHTML = "Please enter a number greater than or equal to 1";
-} else if (inputVal >= 4000) {
-    output.innerHTML = "Please enter a number less than or equal to 3999";
-} else if (isNaN(inputVal))
-{
-  output.innerHTML = "Please enter a valid number";  
-}
-})
+    } else if (inputVal < 0) {
+        output.innerHTML = "Please enter a number greater than or equal to 1";
+    } else if (inputVal >= 4000) {
+        output.innerHTML = "Please enter a number less than or equal to 3999";
+    } else if (isNaN(inputVal)) {
+        output.innerHTML = "Please enter a valid number";  
+    }
+});
 
 output.innerHTML = numeral; 
